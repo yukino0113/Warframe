28fa052a671c2a2d607aa311bc27b9595cd965d7
Yukino0113 <jethro0113@gmail.com>
2024-03-24 10:12:17 +0800

    Update

--- Warframe/drop/parser.py
+++ Warframe/drop/parser.py
@@ -1,22 +1,26 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 from icecream import ic
 
 class Parser:
     def __init__(self):
         '''
-        self.response = requests.get('https://www.warframe.com/zh-hant/droptables')
+        self.response = requests.get('https://www.warframe.com/zh-hant/droptables')a
         self.status = self.response.status_code
         self.content = self.response.text
         '''
         self.soup = self.get_soup()
         self.update_date = self.soup.find('p').text.split(':')[-1].strip()
-
+        for child in self.soup.html.body:
+            if not isinstance(child, NavigableString):
+                print(child.string)
 
     @staticmethod
     def get_soup():
         with open(r'C:\Users\jethr\PycharmProjects\Warframe\Warframe PC Drops.html', mode='r', encoding='utf-8') as fp:
             return BeautifulSoup(fp, 'html.parser')
+
+
 
 Parser()
 
