5bb276d36d0db1fe2dcd1c948d337f787b1b1e91
Jethro <jethro.conde.lim@linecorp.com>
2025-08-20 16:13:23 +0800

    Refactor: Adjust formatting for better readability and consistency in `search.py`

--- backend/drop/search.py
+++ backend/drop/search.py
@@ -12,10 +12,14 @@ T = TypeVar("T")
 
 
 def get_available_sets() -> list[str]:
-    return [x[0] for x in fetchall('SELECT warframe_set FROM vault_status WHERE status = "1"')]
+    return [
+        x[0]
+        for x in fetchall('SELECT warframe_set FROM vault_status WHERE status = "1"')
+    ]
+
 
 def make_question_string(l: Iterable[T]) -> str:
-    return ','.join('?' for _ in l)
+    return ",".join("?" for _ in l)
 
 
 class DropSearchService:
@@ -23,7 +27,7 @@ class DropSearchService:
 
     def __init__(self, item_int_arr: List[int]) -> None:
         self.item_int_arr = item_int_arr
-        #self.process_search()
+        # self.process_search()
 
     def process_search(self):
         # Step 1: Turn the item list (int) to an item list (str)
@@ -45,8 +49,12 @@ class DropSearchService:
         :rtype: list[str]
         """
         place_holder = make_question_string(lst)
-        query = f'SELECT warframe_set, parts_name FROM prime_parts WHERE id IN ({place_holder})'
-        return [' Prime '.join(x) for x in fetchall(query, lst) if x[0] in get_available_sets()]
+        query = f"SELECT warframe_set, parts_name FROM prime_parts WHERE id IN ({place_holder})"
+        return [
+            " Prime ".join(x)
+            for x in fetchall(query, lst)
+            if x[0] in get_available_sets()
+        ]
 
     @staticmethod
     def search_item_drop(lst: List[str]) -> dict:
@@ -67,8 +75,11 @@ class DropSearchService:
             value is a list of named tuples (relic: str, radiant: str, drop_rate: float)
             containing details about associated relics and their respective drop rates.
         """
-        query_result = fetchall(f'SELECT prize, radiant, drop_rate, relic FROM relic_rewards WHERE prize IN ({make_question_string(lst)})', lst)
-        ItemDropRate = namedtuple('ItemDropRate', ['relic', 'radiant', 'drop_rate'])
+        query_result = fetchall(
+            f"SELECT prize, radiant, drop_rate, relic FROM relic_rewards WHERE prize IN ({make_question_string(lst)})",
+            lst,
+        )
+        ItemDropRate = namedtuple("ItemDropRate", ["relic", "radiant", "drop_rate"])
         item_drop = defaultdict(list)
 
         for prize, radiant, drop_rate, relic in query_result:
@@ -79,7 +90,10 @@ class DropSearchService:
                 item_drop[prize].append(reward)
             else:
                 for i, existing_reward in enumerate(item_drop[prize]):
-                    if existing_reward.relic == relic and drop_rate > existing_reward.drop_rate:
+                    if (
+                        existing_reward.relic == relic
+                        and drop_rate > existing_reward.drop_rate
+                    ):
                         item_drop[prize][i] = reward
                         break
         return dict(item_drop)
@@ -92,4 +106,6 @@ class DropSearchService:
 
 @router.get("")
 async def search_drop(lst: List[int]) -> JSONResponse:
-    return JSONResponse(DropSearchService(lst).process_search())  # Temp return value for preventing warning
+    return JSONResponse(
+        DropSearchService(lst).process_search()
+    )  # Temp return value for preventing warning
